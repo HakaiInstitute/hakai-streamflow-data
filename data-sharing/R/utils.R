3a3ac34e2a8c09e7dd1be5ea6f52ee0204bd8dc4
Sam Albers <sam.albers@hakai.org>
2025-06-18 14:20:15 -0700

    revise to use multiple calls

--- data-sharing/R/utils.R
+++ data-sharing/R/utils.R
@@ -5,8 +5,14 @@
 #'
 #' @param ... Arguments passed to read.csv()
 #' @return Data frame with latest_time and record_time as UTC POSIXct
-read_last_measurements <- function(...) {
-  latest_times <- read.csv(...)
+read_last_measurements <- function() {
+  last_passed_measurements <- here::here("data-sharing/last_passed_measurements.csv")
+  is_present <- file.exists(last_passed_measurements)
+
+  if (!is_present) {
+    stop("No previously measurements", call. = FALSE)
+  }
+  latest_times <- read.csv(last_passed_measurements)
   latest_times$latest_time <- as.POSIXct(latest_times$latest_time, tz = "UTC")
   latest_times$record_time <- as.POSIXct(latest_times$record_time, tz = "UTC")
   latest_times
@@ -21,7 +27,9 @@ read_last_measurements <- function(...) {
 #' @return Invisibly returns the latest times data frame
 record_last_passed_measurements <- function(file_name) {
   df <- nanoparquet::read_parquet(file_name)
+  # find the latest time per station
   latest_times <- aggregate(time ~ station_id, data = df, FUN = max)
+  # then find the earliest of those
   names(latest_times)[2] <- "latest_time"
   latest_times$record_time <- as.POSIXct(
     format(Sys.time(), tz = "UTC"),
@@ -37,39 +45,6 @@ record_last_passed_measurements <- function(file_name) {
   return(invisible(latest_times))
 }
 
-#' Filter measurements to exclude already processed data
-#'
-#' Filters a data frame to only include measurements newer than the last
-#' successfully processed measurements for each station. If no tracking
-#' file exists, returns all measurements.
-#'
-#' @param df Data frame with columns station_id and time
-#' @return Filtered data frame excluding previously processed measurements
-remember_last_passed_measurements <- function(df) {
-  last_passed_measurements <- here::here("data-sharing/last_passed_measurements.csv")
-  is_present <- file.exists(last_passed_measurements)
-
-  if (is_present) {
-    latest_times <- read_last_measurements(last_passed_measurements)
-
-    merged_data <- merge(
-      df,
-      latest_times,
-      by = "station_id",
-      all.x = TRUE
-    )
-    filtered_measurements <- merged_data[
-      is.na(merged_data$latest_time) |
-        merged_data$time > merged_data$latest_time,
-    ]
-    # Remove the latest_time column
-    filtered_measurements$latest_time <- NULL
-  } else {
-    filtered_measurements <- df
-  }
-  return(filtered_measurements)
-}
-
 #' Get the earliest time to query for new measurements
 #'
 #' Finds the maximum (most recent) time from all stations' last processed
@@ -77,8 +52,32 @@ remember_last_passed_measurements <- function(df) {
 #'
 #' @return Character string of the latest measurement time in ISO 8601 format
 remember_min_last_passed_measurement <- function() {
-  last_passed_measurements <- here::here("data-sharing/last_passed_measurements.csv")
-  latest_times <- read_last_measurements(last_passed_measurements)
-  max_time <- max(latest_times$latest_time)
+  latest_times <- read_last_measurements()
+  max_time <- min(latest_times$latest_time)
   format(max_time, "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
+}
+
+
+fetch_station_data <- function(last_measurements) {
+  station_data_list <- mapply(
+    get_station_measurements, 
+    last_measurements$latest_time, 
+    last_measurements$station_id, 
+    SIMPLIFY = FALSE
+  )
+  do.call(rbind, station_data_list)
+}
+
+get_station_measurements <- function(date, station_id) {
+  time_param <- glue::glue("last_updated_lvl_time>={date}")
+  station_id_param <- glue::glue('station_id=\"{station_id}\"')
+  logger::log_info("Querying '{dataset_id}' dataset for {station_id} since {date} (UTC)}")
+
+  rerddap::tabledap(
+      dataset_id,
+      fields = columns,
+      fmt = 'parquet',
+      time_param,
+      station_id_param
+    )
 }
